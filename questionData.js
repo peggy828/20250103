e90ac42dbbcbafc3f071943e3c82fc57130b726a
peggy828 <peggy8347@gmail.com>
2025-01-03 19:28:44 +0800

    Add files via upload

---
+++ questionData.js
@@ -0,0 +1,63 @@
+const quizData = [
+    {
+        type: "fill",
+        question: "「口吐□□」，指說話有文采，空格中應填入？",
+        answer: "蓮花"
+    },
+    {
+        type: "choice",
+        question: "「六出冰花」是指什麼？",
+        options: ["蓮花", "雪花", "楊花", "桂花"],
+        answer: "雪花"
+    },
+     {
+       type: "choice",
+        question: "「油漬□□」形容油污斑斑的樣子，空格中應填入？",
+        options: ["桃花", "杏花", "麻花", "杜鵑"],
+        answer: "麻花"
+    },
+     {
+    type: "choice",
+    question: "「驛寄□□」喻向遠方友人表達思念，空格中應填入?",
+    options: ["杏花", "桃花", "桂花", "梅花"],  // 修正後的版本
+    answer: "梅花"
+      },
+     {
+       type: "choice",
+        question: "「□□椿樹」指父母，空格中應填入?",
+        options: ["萱花", "梨花", "荷花", "櫻花"],
+           answer: "萱花"
+    },
+     {
+       type: "choice",
+        question: "「□□帶雨」，形容女子的嬌美，空格中應填入?",
+        options: ["櫻花", "杏花", "梨花", "山茶"],
+        answer: "梨花"
+    },
+     {
+       type: "choice",
+        question: "「□□瑤草」，形容晶瑩美麗的花草，空格中應填入?",
+        options: ["鈴蘭", "芝蘭", "瓊花", "琪花"],
+        answer: "琪花"
+    },
+    {
+        type: "fill",
+        question: "「□□向日」，為下級對上級表忠誠與仰慕，空格中應填入?",
+        answer: "葵花"
+    },
+     {
+        type: "fill",
+        question: "「□□年華」，指女子二十四歲，空格中應填入?",
+        answer: "花信"
+    },
+     {
+        type: "fill",
+        question: "「明日□□」，比喻過時的事物，空格中應填入?",
+        answer: "黃花"
+    },
+     {
+        type: "fill",
+        question: "「□□一現」，指美好事物很快消逝，空格中應填入?",
+        answer: "曇花"
+      }
+];
